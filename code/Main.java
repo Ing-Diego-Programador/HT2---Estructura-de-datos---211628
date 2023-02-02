fbfaaf126098feb5f28d1d97b9b005e16c395e90
Diego <vas211628@uvg.edu.gt>
2023-02-01 19:12:47 -0600

    Subida de archivos

---
+++ code/Main.java
@@ -0,0 +1,41 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Main {
+    
+     public static void main(String[] args) throws IOException{
+        
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ingrese la ruta de acceso del archivo Postfix:");
+        String direction = scan.nextLine();
+        ArrayList<String> operations = new ArrayList<>();
+        
+        
+        try { //verificar la existencia del archivo
+            BufferedReader bf = new BufferedReader(new FileReader (direction));
+            String line;
+            while((line = bf.readLine()) != null){
+                operations.add(line);
+            }
+         
+           for(String expresion: operations){
+            Lector dec = new Lector();
+            System.out.println("Resultado: " + expresion);
+            System.out.println(dec.DecodeExpresion(expresion));
+            }
+        }
+        catch(FileNotFoundException f){
+            System.out.println("No se encontró el archivo");
+            System.exit(0);
+        }
+
+    }
+
+}
